b3121b6f52954da00bfef1a4495bc6de625483ec
Karateev Maxim <karateev.maxim@gmail.com>
2021-01-05 23:34:24 +0200

    add css to LoginPage

--- client/src/LoginPage/Login.jsx
+++ client/src/LoginPage/Login.jsx
@@ -1,38 +1,48 @@
 import React from "react"
 import { useForm } from "react-hook-form"
-import { Link, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { logIn } from "../store/auth/actions"
+import styles from "./Login.module.scss"
+import backward from "../assets/backward.png"
 
 export const Login = () => {
   const history = useHistory()
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, watch } = useForm()
   const dispatch = useDispatch()
 
-  const onSubmit = () => {
+  const onSubmit = (data) => {
+    if (!data.login || !data.password) {
+      return
+    }
     dispatch(logIn())
     history.push("/")
   }
 
+  const selection = watch()
+  const btnClass = `${styles.button} ${
+    selection.login && selection.password
+      ? styles.buttonEnable
+      : styles.buttonDisable
+  }`
+
   return (
-    <>
-      <h3>Аутентификация</h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="login">
-          Логин
+    <div className={styles.background}>
+      <div className={styles.header}>
+        <img src={backward} alt="" />
+        <span>Авторизация</span>
+      </div>
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="login">Логин</label>
           <input id="login" type="text" name="login" ref={register} />
-        </label>
-        <br />
-        <label htmlFor="password">
-          Пароль
+          <label htmlFor="password">Пароль</label>
           <input id="password" type="text" name="password" ref={register} />
-        </label>
-        <br />
-        <br />
-        <button type="submit">Вход</button>
-      </form>
-      <br />
-      <Link to="/registration">Регистрация</Link>
-    </>
+          <button className={btnClass} type="submit">
+            Войти
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
