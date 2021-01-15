e83c3e06c35987dd43f6cc8560f48838074fd7f5
Karateev Maxim <karateev.maxim@gmail.com>
2021-01-15 20:02:23 +0200

    add css to OrderPage

--- client/src/sharedComponents/OrderPreview.jsx
+++ client/src/sharedComponents/OrderPreview.jsx
@@ -1,17 +1,44 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styles from "./OrderPreview.module.scss"
+import repeatIcon from "../assets/repeat.svg"
+import deliveringIcon from "../assets/icn_delivery.svg"
 
-export const OrderPreview = ({ name, description }) => {
+export const OrderPreview = ({
+  orderNumber,
+  orderDate,
+  pizzaName,
+  pizzaDescription,
+  price,
+  delivering,
+}) => {
+  const icon = delivering ? deliveringIcon : repeatIcon
   return (
     <div className={styles.container}>
-      <p>{name}</p>
-      <div>{description}</div>
+      <div className={styles.header}>
+        <div>Заказ {orderNumber}</div>
+        <div className={styles.date}>{orderDate}</div>
+      </div>
+      <div className={styles.title}>{pizzaName}</div>
+      <div className={styles.description}>{pizzaDescription}</div>
+      <div className={styles.footer}>
+        <div>{`${price} руб`}</div>
+        <div className={styles.status}>
+          <img src={icon} alt="" />
+          <div className={delivering ? styles.delivering : styles.repeat}>
+            {delivering ? "Доставляется" : "Повторить заказ"}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 OrderPreview.propTypes = {
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  orderNumber: PropTypes.string.isRequired,
+  orderDate: PropTypes.string.isRequired,
+  pizzaName: PropTypes.string.isRequired,
+  pizzaDescription: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  delivering: PropTypes.bool.isRequired,
 }
